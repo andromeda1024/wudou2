e88a9a57d6d72e15b4e2b86ca643c844555b29c3
高洁 <tj.gaojie@outlook.com>
2017-10-11 12:37:54 +0800

    init f7 version

---
+++ app/wudou.js
@@ -0,0 +1,10 @@
+var wdApp = new Framework7();
+var $$ = Dom7;
+
+var mainView = wdApp.addView('.view-main', {
+    dynamicNavbar: true
+});
+
+wdApp.onPageInit('about', function (page) {
+        
+});
